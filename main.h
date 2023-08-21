2df30e55fab6281146eeeb9650d8e444a89e0c77
Ayoyinkaaaa <Egunjobikao@gmail.com>
2023-08-21 10:37:01 -0700

    task

--- main.h
+++ main.h
@@ -1,10 +1,13 @@
 #ifndef PRINTF_H
 #define PRINTF_H
 #include <stdarg.h>
+#include <stdio.h>
 int _printf(const char *format, ...);
 int print_format(const char *format, va_list arg);
 int c_handle(va_list arg);
 int s_handle(va_list arg);
 int percent_handle(va_list arg);
 int d_handle(va_list arg);
+void print_binary(unsigned int num);
+int main(void);
 #endif
